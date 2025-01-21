7f63fda71893f628595541089117c107f4807a63
owalurso <137452604+owalurso@users.noreply.github.com>
2025-01-21 10:28:03 -0500

    Update nodejsDockerDSL.groovy
    Actualizar Conf

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
-job('Aplicacion Node.js Docker DSL') {
+job('AplicacionNodeJS_DockerDSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/owalurso/nodejs_dsl.git', 'main') { node ->
+            node / gitConfigName('owalurso')
+            node / gitConfigEmail('owalurso@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('oviedus/appnodejs')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
